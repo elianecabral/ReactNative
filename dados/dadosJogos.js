eacb038903f6df61e6854537c4af27df512e64fd
unknown <you@example.com>
2022-04-12 10:31:38 -0300

    versão flatlist

---
+++ dados/dadosJogos.js
@@ -0,0 +1,60 @@
+const Jogos = [
+    {
+        id : "01",
+        nome : "Subway Surfers",
+        valor : "R$13,32",
+        imagem : 'subway.jpg'
+    },
+    {
+        id : "01",
+        nome : "Yugi Master Duel",
+        valor : "Gratuito",
+        imagem : 'yugi.jpg'
+    },
+    {
+        id : "03",
+        nome : "Pokemon Go",
+        valor : 'Gratuito',
+        imagem : 'pokemon.jpg'
+    },
+    {
+        id : "04",
+        nome : "Hora da Aventura",
+        valor : 'R$10,00',
+        imagem : 'aventura.jpg'
+    },
+    {
+        id : "05",
+        nome : "Dragon Ball Kakarot",
+        valor : 'R$18,00',
+        imagem : 'dbz.jpg',
+    },
+    {
+        id : "06",
+        nome : "Zombie Tsunami",
+        valor : "Gratuito",
+        imagem : 'zombie.jpg',
+    },
+    {
+        id : "07",
+        nome : "Pac-Man",
+        valor : "R$2,99",
+        imagem : 'pacman.jpg',
+    },
+    {
+        id : "08",
+        nome : "Splatoon",
+        valor : "R$0,99",
+        imagem : 'CapaSplatoon.jpg',
+    },
+    {
+        id : "07",
+        nome : "Mario Party Superstars",
+        valor : "Gratuito",
+        imagem : 'mario.jpg',
+    },
+        
+    
+]
+
+export default Jogos;
