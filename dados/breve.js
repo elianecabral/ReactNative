025611d184d502e9948b26ca6487142df85c0cf7
ELIANE DOS REIS CABRAL DA ROCHA <75854127+elianecabral@users.noreply.github.com>
2022-04-26 08:04:42 -0300

    Add files via upload

---
+++ dados/breve.js
@@ -0,0 +1,25 @@
+const breve =[
+
+    {
+        id:'01',
+        nome:'Mario Party Superstars',
+        data:'em breve',
+        imagem:'mario.jpg'
+
+    },
+    {
+        id:'02',
+        nome:'Splatoon 3',
+        data:'em breve',
+        imagem:'CapaSplatoon.jpg'
+    },
+    {
+        id:'03',
+        nome:'Pacman Mobile',
+        data:'28/03/22',
+        imagem:'pacman.jpg'
+    }
+
+
+]
+export default breve;
---
+++ dados/index.js
@@ -0,0 +1,41 @@
+ const DATA =
+ 
+ 
+ [
+    
+
+    {
+        id:'01',
+        nome:'Subways suffers',
+        preço:'gratuito',
+        imagem:'subway.jpg'
+
+    },
+    {
+        id:'02',
+        nome:'Yugioh Master duel',
+        preço:'gratuito',
+        imagem:'yugi.jpg'
+    },
+    {
+        id:'03',
+        nome:'Pokemon go',
+        preço:'gratuito',
+        imagem:'pokemon.jpg'
+    },
+    {
+        id:'04',
+        nome:'Hora da aventura',
+        preço:'R$10,00',
+        imagem:'aventura.jpg'
+    },
+    {
+        id:'05',
+        nome:'Dragon Ball Fight',
+        preço:'R$18,00',
+        imagem:'dbz.jpg'
+    }
+
+]
+
+export default DATA;
